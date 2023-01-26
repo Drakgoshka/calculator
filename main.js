00aca8da3cb3587244923cc7c5559a6aa275681f
Drakgoshka <drakgoshka@gmail.com>
2023-01-26 16:20:54 +0300

    калькулятор

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-console.log()
-alert
+function calc(a, b, operation) {
+	if (operation === 'add') {
+		return a + b;
+	}
+	else if (operation === 'substract') {
+		return a - b;
+	}
+	else if (operation === "multi" ) {
+		return a * b;
+	}
+}
+
+console.log(calc(5, 2, 'add'));
+console.log(calc(10, 2, 'substract'));
+console.log(calc(2, 3, 'multi'))
